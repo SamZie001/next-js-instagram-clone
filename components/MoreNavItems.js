3f459ede1e3a77c92806a51ee1d4ce773b107580
SamZie001 <samzie001@gmail.com>
2023-04-12 12:50:34 +0100

    more nav components

--- components/MoreNavItems.js
+++ components/MoreNavItems.js
@@ -1,7 +1,66 @@
+import moreStyles from "../styles/NavComponents/MoreNavItems.module.css";
+
 const MoreNavItems = () => {
   return (
-    <div>MoreNavItems</div>
-  )
-}
+    <div className={moreStyles.main}>
+      <ul className={moreStyles.child}>
+        <div className={moreStyles.list}>
+          <li>
+            <i className="fa-solid fa-gear"></i>
+            <p>Settings</p>
+          </li>
+          <li>
+            <i className="fa-solid fa-greater-than"></i>
+          </li>
+        </div>
+        <div className={moreStyles.list}>
+          <li>
+            <i className="fa-regular fa-clock"></i>
+            <p>Your activity</p>
+          </li>
+          <li>
+            <i className="fa-solid fa-greater-than"></i>
+          </li>
+        </div>
+        <div className={moreStyles.list}>
+          <li>
+            <i className="fa-regular fa-bookmark"></i>
+            <p>Saved</p>
+          </li>
+          <li>
+            <i className="fa-solid fa-greater-than"></i>
+          </li>
+        </div>
+        <div className={moreStyles.list}>
+          <li>
+            <i className="fa-regular fa-moon"></i>
+            <p>Switch appearance</p>
+          </li>
+          <li>
+            <i className="fa-solid fa-greater-than"></i>
+          </li>
+        </div>
+        <div className={moreStyles.list}>
+          <li>
+            <i className="fa-solid fa-circle-exclamation"></i>
+            <p>Report a problem</p>
+          </li>
+          <li>
+            <i className="fa-solid fa-greater-than"></i>
+          </li>
+        </div>
+      </ul>
+      <ul className={moreStyles.child}>
+        <li className={moreStyles.list}>
+          <p>Switch accounts</p>
+          <i className="fa-solid fa-greater-than"></i>
+        </li>
+      </ul>
+      <ul className={moreStyles.child}>
+        <li className={moreStyles.list}>Logout</li>
+      </ul>
+    </div>
+  );
+};
 
-export default MoreNavItems
+export default MoreNavItems;
